d47d64dc78f8ecfb1d2ab5771fd44d292a0b9846
slintristeza <slintristeza@gmail.com>
2019-08-07 00:58:37 +0900

    feat:Edit.tsx

--- sample-editor/src/pages/Edit.tsx
+++ sample-editor/src/pages/Edit.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Edit = () => {
-  const [text, setText] = React.useState('');
+import { Link } from '../Router';
+import { useDatabaseDocument } from '../components/FirebaseDatabase'
+
+const Input = styled.textarea`
+  width: 100%;
+  height: 90vh;
+  ::focus {
+    outline: none;
+  }
+  font-size: 16px;
+  font-family: 'monospace';
+`;
+
+const EditPage: React.FC<{ textId: string }> = props => {
+  const { textId } = props;
+  const { text, updateText, loaded, pending } = useDatabaseDocument(textId);
+
+  if ( !loaded) {
+    return <div>now loading...</div>;
+  }
+
   return (
-    <textarea value={text} onChange={e => setText(e.target.value)} />
+    <>
+      <div>{pending ? 'write pending now' : ''}</div>
+      <Link as = "button" href="/">
+        一覧に戻る
+      </Link>
+      <Input
+        value={text}
+        onChange={e => updateText(e.target.value)}
+        autoFocus
+      />
+    </>
   );
 };
 
-export default Edit;
+export default EditPage;
