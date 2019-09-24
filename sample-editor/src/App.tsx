6bf6e1bf39b7942c54cf78e4502df447ca2a7930
slintristeza <slintristeza@gmail.com>
2019-09-24 22:22:59 +0900

    feat:Route

--- sample-editor/src/App.tsx
+++ sample-editor/src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import { History } from 'history';
+import { History } from 'history'
 
-import { FirebaseAuth, signInWithRedirect, signOut } from './FirebaseAuth';
+import { FirebaseAuth, signInWithRedirect, signOut } from './FirebaseAuth'
 
-import { Router } from './Router';
+import { Router } from './Router'
 
-const Content: React.FC<{ history: History}> = ({ history }) => {
-  return <Router history={history} />;
+const Content: React.FC<{ history: History }> = ({ history }) => {
+  return <Router history={history} />
 }
 
-const App: React.FC<{ history: History}> = ({ history }) => {
+const App: React.FC<{ history: History }> = ({ history }) => {
   const NotSignedIn = React.useCallback(() => {
     return <button onClick={() => signInWithRedirect()}>signIn</button>
-  }, []);
+  }, [])
   const Loading = React.useCallback(() => {
-    return <div>loading now...</div>;
-  }, []);
-  
+    return <div>loading now....</div>
+  }, [])
+
   return (
     <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
       <Content history={history} />
       <button onClick={signOut}>sign out</button>
     </FirebaseAuth>
   )
-};
+}
 
-export default App;
+export default App
--- sample-editor/src/Router.tsx
+++ sample-editor/src/Router.tsx
@@ -1,47 +1,63 @@
-import React from 'react';
-import { History } from 'history';
-import uuidv4 from 'uuidv4';
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useContext,
+  useCallback
+} from 'react'
+import { History } from 'history'
+import uuidv4 from 'uuidv4'
 
-import Edit from './pages/Edit';
 import Index from './pages/Index'
+import Edit from './pages/Edit'
 
-const RouterContext = React.createContext<History | null>(null);
+const RouterContext = React.createContext<History | null>(null)
 
-type RouterProps = { history: History };
-export const Router: React.FC<RouterProps> = ({ history }) => {
-  const [pathname, setPathname] = React.useState(history!.location.pathname);
-  React.useEffect(() => {
-    const unlisten = history!.listen(location => {
-      setPathname(location.pathname);
-    });
-    return unlisten;
+const useCurrentPath = (history: History) => {
+  const [pathname, setPathname] = useState(history.location.pathname)
+  useEffect(() => {
+    const unlisten = history.listen(location => {
+      setPathname(location.pathname)
+    })
+    return unlisten
   }, [history])
 
-  const content = React.useMemo(() => {
+  return pathname
+}
+
+const useEditorRouting = (pathname: string) => {
+  const content = useMemo(() => {
     if (pathname === '/') {
-      return <Index />;
+      return <Index />
     } else if (uuidv4.is(pathname.slice(1))) {
-      return <Edit textId={pathname.slice(1)} />;
+      return <Edit textId={pathname.slice(1)} />
     } else {
-      return <div>404 not found</div>;
+      return <div>404 not found</div>
     }
-  }, [pathname]);
+  }, [pathname])
+
+  return content
+}
+
+type RouterProps = { history: History }
+export const Router: React.FC<RouterProps> = ({ history }) => {
+  const pathname = useCurrentPath(history)
+  const content = useEditorRouting(pathname)
 
   return (
     <RouterContext.Provider value={history}>{content}</RouterContext.Provider>
-  );
-};
+  )
+}
 
-type LinkProps = { href: string; as?: string };
-
+type LinkProps = { href: string; as?: string }
 export const Link: React.FC<LinkProps> = ({ href, as = 'a', children }) => {
-  const history = React.useContext(RouterContext);
-  const onClick = React.useCallback(
+  const history = useContext(RouterContext)
+  const onClick = useCallback(
     (ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      ev.preventDefault();
-      history!.push(href);
+      ev.preventDefault()
+      history!.push(href)
     },
     [history, href]
-  );
-  return React.createElement(as, { onClick, href }, children);
+  )
+  return React.createElement(as, { onClick, href }, children)
 }
